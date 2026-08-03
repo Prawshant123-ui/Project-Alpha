139d7f3fcce78a23b670c452703c22ac3709db18
Prawshant123-ui <coddieprawshant909@gmail.com>
2026-08-03 21:07:17 +0545

    logging setup and uploading setup completed

--- Server/src/config/env.ts
+++ Server/src/config/env.ts
@@ -4,8 +4,11 @@ import "dotenv/config";
 
 export const env = {
   PORT: process.env.PORT || "5000",
-  JWT_SECRET: ("JWT_SECRET"),
+  JWT_SECRET: process.env.JWT_SECRET,
   NODE_ENV: process.env.NODE_ENV || "development",
   LOG_LEVEL: process.env.LOG_LEVEL || "info",
-  CLIENT_URL: "http://localhost:3000"
+  CLIENT_URL: process.env.CLIENT_URL || "http://localhost:3000",
+  CLOUDINARY_API_KEY:process.env.CLOUDINARY_API_KEY,
+  CLOUDINARY_SECRET_KEY:process.env.CLOUDINARY_SECRET_KEY,
+  CLOUDINARY_CLOUD_NAME:process.env.CLOUDINARY_CLOUD_NAME,
 };

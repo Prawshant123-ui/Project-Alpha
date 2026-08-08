fc846b401877be1c3559b3bd6252463653480300
Prawshant123-ui <coddieprawshant909@gmail.com>
2026-08-08 19:33:47 +0545

    continuing note controller

--- Server/src/controllers/noteController.ts
+++ Server/src/controllers/noteController.ts
@@ -1,3 +1,15 @@
+import type {Request,Response} from "express"
 import {prisma} from "../config/prisma.js"
 import {logger} from "../config/logger.js"
-import {jwt} from "../utils/jwt.js"
+import {signToken,verifyToken} from "../utils/jwt.js"
+
+const createNotes=async(req:Request,res:Response)=>{
+    try {
+        const {title,subject,description,domain,thumbnailImageUrl,videoUrl,notePdfUrl}=req.body
+
+        
+
+    } catch (error) {
+        
+    }
+}
